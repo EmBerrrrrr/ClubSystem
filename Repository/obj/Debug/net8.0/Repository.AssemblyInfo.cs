b3ffc613aa9364128d3fdb60e52acf277990f1e7
EmBerrrrrr <lan18051973@gmail.com>
2025-12-08 21:26:03 +0700

    add reason to request

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ac2c39032810c53f10dffd5cfc6f8cda5fdc1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e078db44f7f8792c7876c993fc69a8f81d8e9b04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
